5f96f9b3b167c37182292e95ba829738dbb96bc8
snehaeklure <snehaeklure15@gmail.com>
2024-09-28 12:30:44 +0530

    Application.java file added

---
+++ Application.java
@@ -0,0 +1,58 @@
+package com.mishra;
+
+import java.lang.*; //including Java packages used by this program
+import java.sql.*;
+import com.mishra.*;
+
+public class Account
+{
+	private String Username, Password, Password1, Name;
+
+	public Account(String UN, String PassW, String PassW1, String NM) {
+		Username = UN;
+		Password = PassW;
+		Password1 = PassW1;
+		Name = NM;
+	}
+
+	public Account(String UN, String PassW) {
+		Username = UN;
+		Password = PassW;
+	}
+
+    public boolean signUp() {
+		boolean done = !Username.equals("") && !Password.equals("") && !Password1.equals("") && Password.equals(Password1);
+		try {
+		    if (done) {
+		        DBConnection ToDB = new DBConnection(); //Have a connection to the DB
+		        Connection DBConn = ToDB.openConn();
+		        Statement Stmt = DBConn.createStatement();
+		        String SQL_Command = "SELECT Username FROM Account WHERE Username ='"+Username+"'"; //SQL query command
+		        ResultSet Rslt = Stmt.executeQuery(SQL_Command); //Inquire if the username exsits.
+		        done = done && !Rslt.next();
+		        if (done) {
+				    SQL_Command = "INSERT INTO Account(Username, Password, Name) VALUES ('"+Username+ "','"+Password+"','"+Name+"')"; //Save the username, password and Name
+				    Stmt.executeUpdate(SQL_Command);
+			    }
+			    Stmt.close();
+			    ToDB.closeConn();
+			}
+		}
+	    catch(java.sql.SQLException e)
+	    {         done = false;
+				 System.out.println("SQLException: " + e);
+				 while (e != null)
+				 {   System.out.println("SQLState: " + e.getSQLState());
+					 System.out.println("Message: " + e.getMessage());
+					 System.out.println("Vendor: " + e.getErrorCode());
+					 e = e.getNextException();
+					 System.out.println("");
+				 }
+	    }
+	    catch (java.lang.Exception e)
+	    {         done = false;
+				 System.out.println("Exception: " + e);
+				 e.printStackTrace ();
+	    }
+	    return done;
+	}
